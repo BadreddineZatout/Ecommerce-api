8ed835fa6a3a7763e34c1dfb0b916ceaece70a3d
Badreddine Zatout <hb_zatout@esi.dz>
2023-01-21 23:35:38 +0100

    feat: http exception filter

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { User, UserSchema } from '../../schemas';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
+import { UsersService } from './services/users/users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [],
-  providers: [],
+  providers: [UsersService],
 })
 export class UsersModule {}
